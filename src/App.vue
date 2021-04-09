38f45ad4f494b68e5f51c6aab4527e35b6321d32
Ran Tayar <tgmrp.dev@gmail.com>
2021-04-10 01:56:43 +0300

    card has front and back and can be flipped

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,17 @@
   <section class="game-board">
     <Card
       v-for="(card, index) in cardList"
-      :value="card"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
       :key="`card-${index}`"
+      @select-card="flipCard"
     />
   </section>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 import Card from './components/Card';
 
 export default {
@@ -18,14 +22,23 @@ export default {
     Card,
   },
   setup() {
-    const cardList = [];
+    const cardList = ref([]);
 
     for (let i = 0; i < 16; i++) {
-      cardList.push(i);
+      cardList.value.push({
+        value: i,
+        visible: false,
+        position: i,
+      });
     }
+
+    const flipCard = (payload) => {
+      cardList.value[payload.position].visible = true;
+    };
 
     return {
       cardList,
+      flipCard,
     };
   },
 };
@@ -39,10 +52,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.card {
-  border: 5px solid #ccc;
 }
 
 .game-board {
